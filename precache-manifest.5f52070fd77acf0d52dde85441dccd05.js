0eb75007fef82046938f84f10ffb498c7b86c1e4
David Stewart <darkorin00@hotmail.com>
2020-08-18 18:27:12 -0400

    react deploy

---
+++ precache-manifest.5f52070fd77acf0d52dde85441dccd05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d79f773be452ae66cdf376fbfa5b89",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "a561dff5556d308c1be3",
+    "url": "/employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "594a919bb2c02d6ad0e8",
+    "url": "/employee-directory/static/js/2.22efb6e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.22efb6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a561dff5556d308c1be3",
+    "url": "/employee-directory/static/js/main.ed2e61c0.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
--- src/components/PageHeader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function PageHeader() {
-  return (
-    <h1 className="jumbotron text-center">Employee Directory</h1>
-  )
-}
-
-export default PageHeader;
